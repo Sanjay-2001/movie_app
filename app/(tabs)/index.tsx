6e745e09034023ea8f40298a48e9fb64955f5c67
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-03-16 09:59:51 +0530

    DEV: Home svreen ui and status bar color handled for light and dark

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, Text, View } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
 const index = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+          <SearchBar
+            placeholder="Search for a movie"
+            onPress={() => router.push("/search")}
+          />
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default index
-
-const styles = StyleSheet.create({})
+export default index;
