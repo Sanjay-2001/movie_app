5d7406abbfc6eecee26d2871d24b0bb18bbcedcc
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-03-16 17:28:03 +0530

    DEV: trending page and status bar hidden

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,9 +14,18 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 const index = () => {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
+
   const {
     data: movies,
     loading: moviesLoading,
@@ -35,14 +44,14 @@ const index = () => {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
@@ -50,6 +59,27 @@ const index = () => {
               onPress={() => router.push("/search")}
             />
             <>
+              {trendingMovies && (
+                <View className="mt-10">
+                  <Text className="text-lg text-white font-bold mb-3">
+                    Trending Movies
+                  </Text>
+                  <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    className="mb-4 mt-3"
+                    data={trendingMovies}
+                    contentContainerStyle={{
+                      gap: 26,
+                    }}
+                    renderItem={({ item, index }) => (
+                      <TrendingCard movie={item} index={index} />
+                    )}
+                    keyExtractor={(item) => item.movie_id.toString()}
+                    ItemSeparatorComponent={() => <View className="w-4" />}
+                  />
+                </View>
+              )}
               <Text className="text-lg text-white font-bold mt-5 mb-3 ">
                 Latest Movies
               </Text>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from "react-native";
 export default function RootLayout() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#030014" />
+      <StatusBar hidden={true} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
